03027496a658a2d10f53c12af04877cc174ab650
pepe132 <jose.garciadom4gmail.com>
2022-11-28 13:16:02 -0600

    Cambio de alojamiento de servidor

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const Server = require('./models/server');
 require('dotenv').config();
 
-const http =require('http');
 
-setInterval(function() {
-    http.get("http://byfeliz.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
 
 const server=new Server();
 
